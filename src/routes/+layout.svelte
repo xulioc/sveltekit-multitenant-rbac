06595997fa0b9e6a1c9fa3a93ed051effb61e4b6
Xulio Coira <xulioc@gmail.com>
2024-07-23 19:21:12 +0200

    fix umami

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { env } from '$env/dynamic/public';
-	import { PUBLIC_UMAMI_URL, PUBLIC_UMAMI_WEBID } from '$env/static/public';
+
 	import { UmamiAnalytics } from '@lukulent/svelte-umami';
 	import { ModeWatcher, mode } from 'mode-watcher';
 	import { Toaster } from 'svelte-sonner';
@@ -11,8 +11,8 @@
 <ModeWatcher />
 <Toaster richColors theme={$mode} />
 
-{#if !dev && 'PUBLIC_UMAMI_URL' in env}
-	<UmamiAnalytics websiteID={PUBLIC_UMAMI_WEBID} srcURL={PUBLIC_UMAMI_URL} />
+{#if !dev && env.PUBLIC_UMAMI_URL && env.PUBLIC_UMAMI_WEBID}
+	<UmamiAnalytics websiteID={env.PUBLIC_UMAMI_WEBID} srcURL={env.PUBLIC_UMAMI_URL} />
 {/if}
 
 <slot />
