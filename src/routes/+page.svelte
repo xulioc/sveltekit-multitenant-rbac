b2e0378f34642f43d1e73e23c99fd182cb9fbbbd
Xulio Coira <xulioc@gmail.com>
2024-09-22 19:01:03 +0200

    add github icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,6 +32,7 @@
 			>
 				<LayoutGridIcon class="h-5 w-5 transition-all group-hover:scale-75" />
 			</a>
+
 			<p class="text-xl font-semibold">
 				{#if 'PUBLIC_APP_NAME' in env}
 					{env.PUBLIC_APP_NAME}
@@ -42,6 +43,28 @@
 		</nav>
 
 		<div class="ml-auto grid grid-flow-col gap-3">
+			<Button
+				variant="outline"
+				target="_blank"
+				href="https://github.com/xulioc/sveltekit-multitenant-rbac"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-github"
+					><path
+						d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"
+					/><path d="M9 18c-4.51 2-5-2-7-2" /></svg
+				>
+			</Button>
+
 			<Button on:click={() => goto('/auth/sign-in')}>Sign In</Button>
 			<Button on:click={() => goto('/auth/sign-up')} variant="outline">Sign Up</Button>
 			<LightSwitch></LightSwitch>
